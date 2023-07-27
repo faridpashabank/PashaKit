922e036605834d573c9772648b45e18da9dbc230
Farid Valiyev <91952345+faridpashabank@users.noreply.github.com>
2023-07-27 17:08:04 +0400

    Update PBBRoundButton.swift

--- Sources/PashaKit/PBUIButton/PBBRoundButton.swift
+++ Sources/PashaKit/PBUIButton/PBBRoundButton.swift
@@ -213,7 +213,7 @@ public class PBBRoundButton: UIView {
         self.addSubview(view)
 
         view.axis = .vertical
-        view.distribution = .equalSpacing
+        view.distribution = .fillProportionally
         view.spacing = 0
 
         view.translatesAutoresizingMaskIntoConstraints = false
