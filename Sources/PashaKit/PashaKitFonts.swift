0a180e6756615a1249fadde11fb6b6726a1de3b9
Farid Valiyev <91952345+faridpashabank@users.noreply.github.com>
2023-08-01 14:47:13 +0400

    Update PashaKitFonts.swift

--- Sources/PashaKit/PashaKitFonts.swift
+++ Sources/PashaKit/PashaKitFonts.swift
@@ -11,20 +11,20 @@ import UIKit
 public struct PashaKitFonts {
      public static func registerSFProDisplayFonts() {
          SFProDisplayWeight.allCases.forEach {
-            registerFont(bundle: .module, fontName: $0.rawValue, fontExtension: "ttf")
+            registerFont(bundle: .module, fontName: $0.rawValue, fontExtension: "otf")
         }
      }
     
     public static func registerSFProTextFonts() {
         SFProTextWeight.allCases.forEach {
-           registerFont(bundle: .module, fontName: $0.rawValue, fontExtension: "ttf")
+           registerFont(bundle: .module, fontName: $0.rawValue, fontExtension: "otf")
        }
     }
 
     fileprivate static func registerFont(bundle: Bundle, fontName: String, fontExtension: String) {
 
         print("bundle: \(bundle)")
-        print("font: \(fontName +"."+ fontExtension)")
+        print("font: \(fontName)")
         
         guard let fontURL = bundle.url(forResource: fontName, withExtension: fontExtension),
               let fontDataProvider = CGDataProvider(url: fontURL as CFURL),
