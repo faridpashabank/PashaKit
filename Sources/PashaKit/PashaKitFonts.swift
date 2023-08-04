0b3f1426f4ff7a5c937693758b1f7be8eb206a1a
Farid <farid.valiyev@pashabank.az>
2023-08-04 18:40:16 +0400

    fonts-removed

--- Sources/PashaKit/PashaKitFonts.swift
+++ Sources/PashaKit/PashaKitFonts.swift
@@ -1,39 +1,39 @@
+////
+////  PashaKitFonts.swift
+////
+////
+////  Created by Farid Valiyev on 31.07.23.
+////
 //
-//  PashaKitFonts.swift
-//  
+//import Foundation
+//import UIKit
 //
-//  Created by Farid Valiyev on 31.07.23.
+//public struct PashaKitFonts {
+//     public static func registerSFProDisplayFonts() {
+//         SFProDisplayWeight.allCases.forEach {
+//            registerFont(bundle: .module, fontName: $0.rawValue, fontExtension: "otf")
+//        }
+//     }
 //
-
-import Foundation
-import UIKit
-
-public struct PashaKitFonts {
-     public static func registerSFProDisplayFonts() {
-         SFProDisplayWeight.allCases.forEach {
-            registerFont(bundle: .module, fontName: $0.rawValue, fontExtension: "otf")
-        }
-     }
-    
-    public static func registerSFProTextFonts() {
-        SFProTextWeight.allCases.forEach {
-           registerFont(bundle: .module, fontName: $0.rawValue, fontExtension: "otf")
-       }
-    }
-
-    fileprivate static func registerFont(bundle: Bundle, fontName: String, fontExtension: String) {
-
-//        print("bundle: \(bundle)")
-//        print("font: \(fontName)")
-        
-        guard let fontURL = bundle.url(forResource: fontName, withExtension: fontExtension),
-              let fontDataProvider = CGDataProvider(url: fontURL as CFURL),
-              let font = CGFont(fontDataProvider) else {
-                  fatalError("Couldn't create font from data")
-        }
-
-        var error: Unmanaged<CFError>?
-
-        CTFontManagerRegisterGraphicsFont(font, &error)
-    }
-}
+//    public static func registerSFProTextFonts() {
+//        SFProTextWeight.allCases.forEach {
+//           registerFont(bundle: .module, fontName: $0.rawValue, fontExtension: "otf")
+//       }
+//    }
+//
+//    fileprivate static func registerFont(bundle: Bundle, fontName: String, fontExtension: String) {
+//
+////        print("bundle: \(bundle)")
+////        print("font: \(fontName)")
+//
+//        guard let fontURL = bundle.url(forResource: fontName, withExtension: fontExtension),
+//              let fontDataProvider = CGDataProvider(url: fontURL as CFURL),
+//              let font = CGFont(fontDataProvider) else {
+//                  fatalError("Couldn't create font from data")
+//        }
+//
+//        var error: Unmanaged<CFError>?
+//
+//        CTFontManagerRegisterGraphicsFont(font, &error)
+//    }
+//}
