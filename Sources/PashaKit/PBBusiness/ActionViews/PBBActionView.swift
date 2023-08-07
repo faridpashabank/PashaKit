bd7a38317d463ce217d5ea31e9db66d677d901cb
Farid <farid.valiyev@pashabank.az>
2023-08-07 13:27:17 +0400

    Update PBBActionView.swift

--- Sources/PashaKit/PBBusiness/ActionViews/PBBActionView.swift
+++ Sources/PashaKit/PBBusiness/ActionViews/PBBActionView.swift
@@ -436,7 +436,7 @@ public class PBBActionView: UIView {
         case .description:
             NSLayoutConstraint.activate([
 //                self.heightAnchor.constraint(equalToConstant: 112.0),
-                self.heightAnchor.constraint(equalTo: self.baseView.heightAnchor, constant: self.descriptionLabel.intrinsicContentSize.height + 8),
+                self.heightAnchor.constraint(equalTo: self.baseView.heightAnchor, constant:  40),
 //                self.heightAnchor.constraint(equalToConstant: self.baseView.frame.height + self.descriptionLabel.intrinsicContentSize.height + 8),
                 self.descriptionLabel.topAnchor.constraint(equalTo: self.baseView.bottomAnchor, constant: 8.0),
                 self.descriptionLabel.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 16.0),
