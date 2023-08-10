2682078d6f184257ab6bcad491d942b988eba1b6
Farid <farid.valiyev@pashabank.az>
2023-08-10 18:18:33 +0400

    Update PBBTextFieldValidations.swift

--- Sources/PashaKit/PBBusiness/TextFields/Validations/PBBTextFieldValidations.swift
+++ Sources/PashaKit/PBBusiness/TextFields/Validations/PBBTextFieldValidations.swift
@@ -10,6 +10,14 @@ import Foundation
 public class PBBTextFieldValidations {
     static func validateEmail(email: String) -> PBTextFieldValidity {
         print("EMAIL::: \(email)")
-        return .invalid("Fuck you bitch")
+        
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+
+        let emailPred = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
+        if emailPred.evaluate(with: email) {
+            return .valid
+        } else {
+            return .invalid("Email is not correct")
+        }
     }
 }
