32f644458d1f49dc7956600d9bf6b925ddf960a5
Farid <farid.valiyev@pashabank.az>
2023-08-13 19:51:01 +0400

    Update PBBAttentionView.swift

--- Sources/PashaKit/PBBusiness/InfoViews/PBBAttentionView.swift
+++ Sources/PashaKit/PBBusiness/InfoViews/PBBAttentionView.swift
@@ -266,11 +266,11 @@ open class PBBAttentionView: UIView {
             self.infoIcon.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 16.0),
             self.infoIcon.centerYAnchor.constraint(equalTo: self.centerYAnchor),
             
-            self.textStackView.heightAnchor.constraint(equalToConstant: 42),
-            self.textStackView.centerYAnchor.constraint(equalTo:self.centerYAnchor),
+//            self.textStackView.heightAnchor.constraint(equalToConstant: 42),
+//            self.textStackView.centerYAnchor.constraint(equalTo:self.centerYAnchor),
             
-//            self.textStackView.topAnchor.constraint(equalTo: self.topAnchor, constant: 12),
-//            self.textStackView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -12),
+            self.textStackView.topAnchor.constraint(equalTo: self.topAnchor, constant: 12),
+            self.textStackView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -12),
             self.textStackView.leftAnchor.constraint(equalTo: self.infoIcon.rightAnchor, constant: 12),
             self.textStackView.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -16),
         ])
